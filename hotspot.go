e20fc4ba7c6d1ed08f08a50f0858c1acd8aa525b
Rasim Yılmaz <rasimyilmaz@Rasim-iMac.local>
2019-03-20 19:08:38 +0300

    Getting Ready for windows service application

---
+++ hotspot.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	debug    = flag.Bool("debug", false, "enable debugging")
+	password = flag.String("password", "admin1982", "the database password")
+	port     = flag.Int("port", 1433, "the database port")
+	server   = flag.String("server", "SRV", "the database server")
+	user     = flag.String("user", "adminakad", "the database user")
+)
+
+//Guest type contains information check in date , check out date , type of id card , id number, name , birth year
+type Guest struct {
+	CheckInDate  time.Time `json:"check_in_date"`
+	CheckOutDate time.Time `json:"check_out_date"`
+	IDCard       string    `json:"id_card"`
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	BirthYear    int       `json:"birth_year"`
+}
+
+func main() {
+	flag.Parse()
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.GET("/", QueryQuests)
+	r.Run()
+}
+func serveHTTP() {
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":3000", nil)
+}
+
+//QueryQuests Responses Quests with JSON format
+func QueryQuests(c *gin.Context) {
+	response, err := GetGuests(c.Query("name"))
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+	} else {
+		c.JSON(200, response)
+	}
+}
+
+//GetGuests Input parameter name is stand for database name , return value slice of guest stuct and if there is no error , error is nil
+func GetGuests(name string) ([]Guest, error) {
+	ctx := context.Background()
+	var Quests []Guest
+	if *debug {
+		fmt.Printf(" password:%s\n", *password)
+		fmt.Printf(" port:%d\n", *port)
+		fmt.Printf(" server:%s\n", *server)
+		fmt.Printf(" user:%s\n", *user)
+	}
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s", *server, *user, *password, *port, name)
+	if *debug {
+		fmt.Printf(" connString:%s\n", connString)
+	}
+	conn, err := sql.Open("mssql", connString)
+	if err != nil {
+		log.Print("Open connection failed:", err.Error())
+		return Quests, err
+	}
+	err = conn.PingContext(ctx)
+	if err != nil {
+		log.Print(err.Error())
+		return nil, err
+	}
+	defer conn.Close()
+
+	stmt, err := conn.Prepare("SELECT Q.CITARIHI,Q.COUTTARIHI,CASE WHEN M.VATNO is not null then M.VATNO WHEN M.KIMLIKTURU='PASAPORT' THEN ISNULL(M.KIMLIKNO,'YABANCI') ELSE 'NOTSENSE' END as KIMLIKNO,ISNULL(M.KIMLIKTURU,'') AS KIMLIKTURU  ,ISNULL(M.AD,'') +' ' + SUBSTRING(ISNULL(M.SOYAD,''),0,2)+'.' AS ADI,ISNULL(YEAR(M.DOGUMTARIHI),0) AS DOGUMYILI FROM QRES_ALL AS Q INNER JOIN REZKISI R ON R.KNO = Q.KNO LEFT JOIN MISAFIR M ON M.MID = R.MID where Q.DURUM='I' AND NOT Q.ODANO LIKE 'T%' and ( M.KIMLIKTURU is not null or M.VATNO is not null)")
+	if err != nil {
+		log.Print("Prepare failed:", err.Error())
+		return Quests, err
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.QueryContext(ctx)
+	if err != nil {
+		log.Print(err)
+		return Quests, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var row Guest
+		err := rows.Scan(&row.CheckInDate, &row.CheckOutDate, &row.ID, &row.IDCard, &row.Name, &row.BirthYear)
+		if err != nil {
+			log.Print(err)
+		}
+		Quests = append(Quests, row)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Print(err)
+		return Quests, err
+	}
+	return Quests, nil
+}
--- main.go
+++ main.go
@@ -1,111 +1,76 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build windows
+
+// Example service program that beeps.
+//
+// The program demonstrates how to create Windows service and
+// install / remove it on a computer. It also shows how to
+// stop / start / pause / continue any service, and how to
+// write to event log. It also shows how to use debug
+// facilities available in debug package.
+//
 package main
 
 import (
-	"context"
-	"database/sql"
-	"flag"
 	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"os"
+	"strings"
 
-	_ "github.com/denisenkom/go-mssqldb"
-	"github.com/gin-gonic/gin"
+	"golang.org/x/sys/windows/svc"
 )
 
-var (
-	debug    = flag.Bool("debug", false, "enable debugging")
-	password = flag.String("password", "admin1982", "the database password")
-	port     = flag.Int("port", 1433, "the database port")
-	server   = flag.String("server", "SRV", "the database server")
-	user     = flag.String("user", "adminakad", "the database user")
-)
-
-//Guest type contains information check in date , check out date , type of id card , id number, name , birth year
-type Guest struct {
-	CheckInDate  time.Time `json:"check_in_date"`
-	CheckOutDate time.Time `json:"check_out_date"`
-	IDCard       string    `json:"id_card"`
-	ID           string    `json:"id"`
-	Name         string    `json:"name"`
-	BirthYear    int       `json:"birth_year"`
+func usage(errmsg string) {
+	fmt.Fprintf(os.Stderr,
+		"%s\n\n"+
+			"usage: %s <command>\n"+
+			"       where <command> is one of\n"+
+			"       install, remove, debug, start, stop, pause or continue.\n",
+		errmsg, os.Args[0])
+	os.Exit(2)
 }
 
 func main() {
-	flag.Parse()
+	const svcName = "hotspot-users"
 
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	isIntSess, err := svc.IsAnInteractiveSession()
+	if err != nil {
+		log.Fatalf("failed to determine if we are running in an interactive session: %v", err)
+	}
+	if !isIntSess {
+		runService(svcName, false)
+		return
+	}
 
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.GET("/", QueryQuests)
-	r.Run()
-	http.ListenAndServe(":3000", nil)
-}
+	if len(os.Args) < 2 {
+		usage("no command specified")
+	}
 
-//QueryQuests Responses Quests with JSON format
-func QueryQuests(c *gin.Context) {
-	response, err := GetGuests(c.Query("name"))
+	cmd := strings.ToLower(os.Args[1])
+	switch cmd {
+	case "debug":
+		runService(svcName, true)
+		return
+	case "install":
+		err = installService(svcName, "Elektra Guest List Serve Api")
+	case "remove":
+		err = removeService(svcName)
+	case "start":
+		err = startService(svcName)
+	case "stop":
+		err = controlService(svcName, svc.Stop, svc.Stopped)
+	case "pause":
+		err = controlService(svcName, svc.Pause, svc.Paused)
+	case "continue":
+		err = controlService(svcName, svc.Continue, svc.Running)
+	default:
+		usage(fmt.Sprintf("invalid command %s", cmd))
+	}
 	if err != nil {
-		c.JSON(500, gin.H{
-			"message": err.Error(),
-		})
-	} else {
-		c.JSON(200, response)
+		log.Fatalf("failed to %s %s: %v", cmd, svcName, err)
 	}
-}
-
-//GetGuests Input parameter name is stand for database name , return value slice of guest stuct and if there is no error , error is nil
-func GetGuests(name string) ([]Guest, error) {
-	ctx := context.Background()
-	var Quests []Guest
-	if *debug {
-		fmt.Printf(" password:%s\n", *password)
-		fmt.Printf(" port:%d\n", *port)
-		fmt.Printf(" server:%s\n", *server)
-		fmt.Printf(" user:%s\n", *user)
-	}
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s", *server, *user, *password, *port, name)
-	if *debug {
-		fmt.Printf(" connString:%s\n", connString)
-	}
-	conn, err := sql.Open("mssql", connString)
-	if err != nil {
-		log.Print("Open connection failed:", err.Error())
-		return Quests, err
-	}
-	err = conn.PingContext(ctx)
-	if err != nil {
-		log.Print(err.Error())
-		return nil, err
-	}
-	defer conn.Close()
-
-	stmt, err := conn.Prepare("SELECT Q.CITARIHI,Q.COUTTARIHI,CASE WHEN M.VATNO is not null then M.VATNO WHEN M.KIMLIKTURU='PASAPORT' THEN ISNULL(M.KIMLIKNO,'YABANCI') ELSE 'NOTSENSE' END as KIMLIKNO,ISNULL(M.KIMLIKTURU,'') AS KIMLIKTURU  ,ISNULL(M.AD,'') +' ' + SUBSTRING(ISNULL(M.SOYAD,''),0,2)+'.' AS ADI,ISNULL(YEAR(M.DOGUMTARIHI),0) AS DOGUMYILI FROM QRES_ALL AS Q INNER JOIN REZKISI R ON R.KNO = Q.KNO LEFT JOIN MISAFIR M ON M.MID = R.MID where Q.DURUM='I' AND NOT Q.ODANO LIKE 'T%' and ( M.KIMLIKTURU is not null or M.VATNO is not null)")
-	if err != nil {
-		log.Print("Prepare failed:", err.Error())
-		return Quests, err
-	}
-	defer stmt.Close()
-
-	rows, err := stmt.QueryContext(ctx)
-	if err != nil {
-		log.Print(err)
-		return Quests, err
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var row Guest
-		err := rows.Scan(&row.CheckInDate, &row.CheckOutDate, &row.ID, &row.IDCard, &row.Name, &row.BirthYear)
-		if err != nil {
-			log.Print(err)
-		}
-		Quests = append(Quests, row)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Print(err)
-		return Quests, err
-	}
-	return Quests, nil
+	return
 }
